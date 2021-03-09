103d9fbedf6997ddd9c0feff3dc35a26d21dd237
javad <pournosrat.javad@gmail.com>
2021-03-09 23:48:52 +0330

    add new items

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ip-finder
 enter domain name and find ip address
+pip3 install whois
--- socket-ip.py
+++ socket-ip.py
@@ -12,9 +12,14 @@ print('''
 ======================================================================
 ''')
 import socket
+import whois
 host = input(str('enter domain name:'))
+domain = whois.query(host)
 def get_ip(host):
-    print(f'ip is {socket.gethostbyname(host)}')
-
+    print(f'IP: {socket.gethostbyname(host)}')
+    print(socket.gethostbyname_ex(host))
+    print(f'expiration day: {domain.expiration_date}')
+    print(f'registrar: {domain.registrar}')
+    print(f'name servers: {domain.name_servers}')
 get_ip(host)
 
