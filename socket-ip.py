8e0405532fc2c3c6829166c734abc970c02b6865
javad pournosrat <pournosrat.javad@gmail.com>
2021-04-05 10:40:55 +0430

    rm whois module

--- socket-ip.py
+++ socket-ip.py
@@ -12,14 +12,9 @@ print('''
 ======================================================================
 ''')
 import socket
-#import whois
 host = input(str('enter domain name:'))
-#domain = whois.query(host)
 def get_ip(host):
     print(f'IP: {socket.gethostbyname(host)}')
-    #print(socket.gethostbyname_ex(host))
-    #print(f'expiration day: {domain.expiration_date}')
-    #print(f'registrar: {domain.registrar}')
-    #print(f'name servers: {domain.name_servers}')
+    print(socket.gethostbyname_ex(host))
 get_ip(host)
 
